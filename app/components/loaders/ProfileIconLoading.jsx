6023b49d2f137ed02cd47827ae4f2b82b4278950
chirag-9121 <chirag9121.cg@gmail.com>
2024-07-14 01:43:50 +0100

    loader component

---
+++ app/components/loaders/ProfileIconLoading.jsx
@@ -0,0 +1,10 @@
+function ProfileIconLoading() {
+  return (
+    <div className="flex items-center justify-end gap-8">
+      <div className="h-10 w-10 rounded-full bg-white/60"></div>
+      <div className="h-10 w-10 rounded-full bg-white/60"></div>
+    </div>
+  );
+}
+
+export default ProfileIconLoading;
