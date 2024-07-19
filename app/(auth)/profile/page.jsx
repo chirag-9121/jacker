2cf945254c1368e5b72efd14ba44f4237c683007
chirag-9121 <chirag9121.cg@gmail.com>
2024-07-19 04:16:45 +0100

    created profile update form component

--- app/(auth)/profile/page.jsx
+++ app/(auth)/profile/page.jsx
@@ -3,64 +3,15 @@
 import { useState } from "react";
 import { useUserContext } from "@/app/components/UserProvider";
 
-// public assets and icons
-import { IoMdEye } from "react-icons/io";
-import { IoMdEyeOff } from "react-icons/io";
-
 // ui components
-import { LoadingSpinner } from "@/app/components/ui/spinner";
 import { UserAvatar } from "@/app/components/ui/user-avatar";
+import ProfileUpdateForm from "./ProfileUpdateForm";
 
 function Profile() {
   const { user: authuser, userLoading } = useUserContext(); // Global context user to display preloaded data in form
-  const [validated, setValidated] = useState(true); // validating flag for the password and confirm password
-  const [passwordType, setPasswordType] = useState("password"); // To show/hide password
-  const [showPassword, setShowPassword] = useState(true); // To switch b/w open eye and closed eye
   const [isLoading, setIsLoading] = useState(false); // To change button text to Saving...
-  const [user, setUser] = useState({
-    fname: "",
-    lname: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
+  const [error, setError] = useState();
 
-  // Toggle f:n between input type text and password
-  const showPasswordToggler = () => {
-    setPasswordType((prev) => (prev === "password" ? "text" : "password"));
-    setShowPassword(!showPassword);
-  };
-
-  // function to check if password and confirm_password matches, sets validated flag accordingly
-  const validatePassword = (e) => {
-    const { name, value } = e.target;
-    switch (name) {
-      case "password":
-        if (!value) break;
-        else if (user.confirmPassword && value !== user.confirmPassword)
-          setValidated(false);
-        else if (user.confirmPassword && value === user.confirmPassword)
-          setValidated(true);
-        break;
-      case "confirmPassword":
-        if (!value) break;
-        else if (user.password && value !== user.password) setValidated(false);
-        else if (user.password && value === user.password) setValidated(true);
-        break;
-      default:
-        break;
-    }
-  };
-
-  // function to set password attributes of user object and then validate it
-  const onPasswordChange = (e) => {
-    const { name, value } = e.target;
-    setUser((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-    validatePassword(e);
-  };
   return (
     <section className="flex w-full flex-col items-center p-7 pl-10">
       <p className="self-start text-2xl font-bold dark:text-white">Profile</p>
@@ -76,174 +27,12 @@ function Profile() {
           ) : null}
         </div>
 
-        {/* Profile Update Form */}
-        <form
-          className="flex h-full w-2/3 flex-col justify-between self-end pt-3"
-          action=""
-          // onSubmit={(e) => signupHandler(e, user)} // Callback to page component function
-        >
-          <div className="flex flex-col space-y-4 md:space-y-6">
-            <div className="flex justify-between gap-4">
-              <div className="w-full">
-                <label
-                  htmlFor="first-name"
-                  className="mb-2 block text-xs font-medium text-darkgrey"
-                >
-                  First name
-                </label>
-                <input
-                  type="text"
-                  name="first-name"
-                  id="fname"
-                  onChange={(e) => setUser({ ...user, fname: e.target.value })}
-                  value={user.fname}
-                  className="block w-full rounded-lg border-2 border-transparent bg-darkgrey/10 p-2.5 text-sm text-black focus:border-2 focus:border-primary focus:outline-none focus:ring-0 dark:bg-forminput/10 dark:text-white dark:placeholder-white/50 dark:focus:border-white/70"
-                  placeholder="John"
-                  required={true}
-                />
-              </div>
-
-              <div className="w-full">
-                <label
-                  htmlFor="last-name"
-                  className="mb-2 block text-xs font-medium text-darkgrey"
-                >
-                  Last name
-                </label>
-                <input
-                  type="text"
-                  name="last-name"
-                  id="lname"
-                  onChange={(e) => setUser({ ...user, lname: e.target.value })}
-                  value={user.lname}
-                  className="block w-full rounded-lg border-2 border-transparent bg-darkgrey/10 p-2.5 text-sm text-black focus:border-2 focus:border-primary focus:outline-none focus:ring-0 dark:bg-forminput/10 dark:text-white dark:placeholder-white/50 dark:focus:border-white/70"
-                  placeholder="Doe"
-                  required={true}
-                />
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="email"
-                className="mb-2 block text-xs font-medium text-darkgrey"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                onChange={(e) => setUser({ ...user, email: e.target.value })}
-                value={user.email}
-                className="block w-full rounded-lg border-2 border-transparent bg-darkgrey/10 p-2.5 text-sm text-black focus:border-2 focus:border-primary focus:outline-none focus:ring-0 dark:bg-forminput/10 dark:text-white dark:placeholder-white/50 dark:focus:border-white/70"
-                placeholder="johndoe@gmail.com"
-                required={true}
-              />
-            </div>
-
-            <div className="flex justify-between gap-4">
-              <div className="w-full">
-                <label
-                  htmlFor="password"
-                  className="mb-2 block text-xs font-medium text-darkgrey"
-                >
-                  New password
-                </label>
-                <div className="relative">
-                  <input
-                    type={passwordType}
-                    name="password"
-                    id="password"
-                    onChange={onPasswordChange} // Start validating both password fields when user starts typing
-                    value={user.password}
-                    placeholder="••••••••"
-                    className="block w-full rounded-lg border-2 border-transparent bg-darkgrey/10 p-2.5 text-sm text-black focus:border-2 focus:border-primary focus:outline-none focus:ring-0 dark:bg-forminput/10 dark:text-white dark:placeholder-white/50 dark:focus:border-white/70"
-                    required={true}
-                  />
-                  <IoMdEye
-                    size={20}
-                    style={{ display: showPassword ? "block" : "none" }}
-                    onClick={showPasswordToggler}
-                    className="absolute end-0 top-0 m-2.5 cursor-pointer dark:fill-white"
-                  />
-
-                  <IoMdEyeOff
-                    size={20}
-                    style={{ display: !showPassword ? "block" : "none" }}
-                    onClick={showPasswordToggler}
-                    className="absolute end-0 top-0 m-2.5 cursor-pointer dark:fill-white"
-                  />
-                </div>
-              </div>
-              <div className="w-full">
-                <label
-                  htmlFor="confirm-password"
-                  className="mb-2 block text-xs font-medium text-darkgrey"
-                >
-                  Confirm password
-                </label>
-                <div className="relative">
-                  <input
-                    type={passwordType}
-                    name="confirmPassword"
-                    id="confirm-password"
-                    onChange={onPasswordChange}
-                    value={user.confirmPassword}
-                    placeholder="••••••••"
-                    className="block w-full rounded-lg border-2 border-transparent bg-darkgrey/10 p-2.5 text-sm text-black focus:border-2 focus:border-primary focus:outline-none focus:ring-0 dark:bg-forminput/10 dark:text-white dark:placeholder-white/50 dark:focus:border-white/70"
-                    required={true}
-                  />
-                  <IoMdEye
-                    size={20}
-                    style={{ display: showPassword ? "block" : "none" }}
-                    onClick={showPasswordToggler}
-                    className="absolute end-0 top-0 m-2.5 cursor-pointer dark:fill-white"
-                  />
-
-                  <IoMdEyeOff
-                    size={20}
-                    style={{ display: !showPassword ? "block" : "none" }}
-                    onClick={showPasswordToggler}
-                    className="absolute end-0 top-0 m-2.5 cursor-pointer dark:fill-white"
-                  />
-                </div>
-              </div>
-            </div>
-
-            {/* Password validation error */}
-            {!validated && (
-              // will-change-transform is applied as the color of this p tag was changing based on whether the above input
-              // fields are focused or not
-              <p className="text-end text-sm font-semibold text-error will-change-transform">
-                Passwords do not match
-              </p>
-            )}
-          </div>
-
-          {/* Form action buttons */}
-          <div className="flex items-center justify-end gap-5">
-            <input
-              type="reset"
-              value="Cancel"
-              className="h-9 w-20 cursor-pointer rounded-lg bg-primary/20 text-sm font-semibold text-primary dark:bg-primary/20 dark:text-primary-light/80"
-            />
-            <button
-              type="submit"
-              disabled={!validated && isLoading}
-              className="h-9 w-20 rounded-lg bg-primary text-sm font-semibold text-white hover:bg-primary/80"
-            >
-              {/* Updating text in button based on isLoading value */}
-              {!isLoading && "Save"}
-
-              {isLoading && (
-                <div className="flex items-center justify-center gap-2">
-                  <LoadingSpinner /> Saving...
-                </div>
-              )}
-            </button>
-          </div>
-        </form>
+        <ProfileUpdateForm
+          authuser={authuser}
+          isLoading={isLoading}
+          userLoading={userLoading}
+          error={error}
+        />
       </div>
     </section>
   );
